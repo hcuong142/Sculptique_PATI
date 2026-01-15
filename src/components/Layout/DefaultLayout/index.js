9499ca0a72960bb477ec053faad12f2bd0ce9f04
hcuong142 <huycuong140203@gmail.com>
2026-01-16 00:11:29 +0700

    add header, home page

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,17 +1,12 @@
 import Header from '~/components/Layout/components/Header';
-import Sidebar from './Sidebar';
 import styles from './DefaultLayout.module.scss';
-import classNames from 'classnames/bind';
-
-const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
     return (
-        <div className={cx('wrapper')}>
+        <div >
             <Header />
-            <div className={cx('container')}>
-                <Sidebar />
-                <div className={cx('content')}>{children}</div>
+            <div >
+                <div >{children}</div>
             </div>
         </div>
     );
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,10 @@
 import { HeaderOnly } from '~/components/Layout';
+import { Home } from '~/pages/Home';
 
-import Home from '~/pages/Home';
-import Following from '~/pages/Following';
-import Profile from '~/pages/Profile';
-import Upload from '~/pages/Upload';
-import Search from '~/pages/Search';
-
-// không cần đăng nhập vẫn xem được
 const publicRoutes = [
     { path: '/', component: Home },
-    { path: '/following', component: Following },
-    { path: '/profile', component: Profile },
-    { path: '/search', component: Search, layout: null },
-    { path: '/upload', component: Upload, layout: HeaderOnly },
 ];
 
-// ví dụ phải đăng nhập mới xem được
 const privateRoutes = [];
 
 export { publicRoutes, privateRoutes };
